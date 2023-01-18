f8da34c50d8aa1cb2710bb0f69147009186ca1a6
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-01-18 10:52:56 -0600

    updated index.html completely

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-let now = new Date();
-
-let h2 = document.querySelector("h2");
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-
-h2.innerHTML = ` ${date}, ${hours}:${minutes}, `;
-
-function search(city) {
-  //searchInput.value
-  let apiKey = "f5029b784306910c19746e40c14d6cd3";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSearch(e) {
-  e.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  search(searchInput.value);
-}
-
-let form = document.querySelector("#search-form");
-
 form.addEventListener("submit", handleSearch);
 
 function displayTemperature(response) {
@@ -41,3 +16,8 @@ let city = "sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
+
+let button = document.querySelector("#current-location-button");
+button.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
