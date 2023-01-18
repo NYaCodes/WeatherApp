f70c0b51c88dde053cc2b3d42070f63edbde5441
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-01-18 11:10:00 -0600

    Updated Coded Context

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+h2.innerHTML = ` ${date}, ${hours}:${minutes}, `;
+
+function searchCity(city) {
+  //searchInput.value
+  let apiKey = "f5029b784306910c19746e40c14d6cd3";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearch(e) {
+  e.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  searchCity(searchInput.value);
+}
+
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+
 form.addEventListener("submit", handleSearch);
 
 function displayTemperature(response) {
